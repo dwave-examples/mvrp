f9ec302ac3fa6f9402bdad49c705c00f2aed7cfb
Kate Culver <kculver@dwavesys.com>
2024-04-04 13:53:10 -0700

    Removed map loading feedback

--- dash_html.py
+++ dash_html.py
@@ -168,13 +168,7 @@ def set_html(app):
                                         id="map-tab",
                                         value="map-tab",  # used for switching to programatically
                                         className="tab",
-                                        children=[
-                                            dcc.Loading(
-                                                id="loading",
-                                                type="circle",
-                                                color="#17BEBB",
-                                                children=html.Iframe(id="solution-map")
-                                            ),],
+                                        children=html.Iframe(id="solution-map")
                                     ),
                                     dcc.Tab(
                                         label="Results",
