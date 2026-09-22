a5ee3f12f900e5c21fe2c139bb1ff3b6fd7a0ea7
Kate Culver <kateeculver@gmail.com>
2026-09-22 16:39:02 -0700

    Add comment to indicate that lower is better

--- src/ckmeans.py
+++ src/ckmeans.py
@@ -180,7 +180,7 @@ class CKMeans:
             assignments = self.predict_once(X, demand, capacities)
             score = self._get_score(X, assignments)
 
-            if score < best_score:
+            if score < best_score:  # lower score is better
                 best_score = score
                 best_assignments = assignments
 
